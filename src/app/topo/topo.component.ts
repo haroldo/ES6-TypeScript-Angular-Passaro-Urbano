155f768b04c6294787524d6554d6e13912637a4b
haroldo <haroldomsouza@gmail.com>
2019-06-06 13:06:51 -0300

    teste

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service'
-import { from } from 'rxjs';
+import { from, Subject } from 'rxjs';
 import { ProvidersFeature } from '@angular/core/src/render3';
 import { Oferta } from '../shared/oferta.model'
-import { Observable } from 'rxjs'
+import { Observable,of, } from 'rxjs'
+import { switchMap,debounceTime,distinctUntilChanged} from 'rxjs/operators'
+import { catchError } from 'rxjs/operators'
+
 
 
 @Component({
@@ -15,9 +18,36 @@ import { Observable } from 'rxjs'
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
+
+  private subjectPesquisa: Subject<String> = new Subject<string>()
+
   constructor(private ofertaService:OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa //retorno Ofertas
+    .pipe(debounceTime(1000),
+    distinctUntilChanged(),
+    switchMap((termo:string) => {
+      console.log('requisição Http para API: ',termo)
+      if (termo.trim() === '')
+      {
+        //retornar um observable de array de ofertas vazio 
+        return  of<Oferta[]>([]);
+      }
+        return this.ofertaService.pesquisaOfertas(termo)
+    }),
+    catchError((erro:any) => {
+      console.log(erro)
+      return of<Oferta[]>([])
+    })
+    
+    );
+
+    this.ofertas.subscribe((ofertas:Oferta[]) => {
+      console.log(ofertas)
+     this.ofertas2 = ofertas    
+    })
     
   }
 /*
@@ -27,12 +57,18 @@ export class TopoComponent implements OnInit {
 */
   
   public pesquisa(termoBusca:string):void{
-    this.ofertas = this.ofertaService.pesquisaOfertas(termoBusca)
+   
+   /* this.ofertas = this.ofertaService.pesquisaOfertas(termoBusca)
     console.log(this.ofertas)
     this.ofertas.subscribe(
       (ofertas:Oferta[]) => console.log(ofertas),
       (erro:any) => console.log('Erro status: ', erro.status),
       () => console.log('Fluxo de evento completo!')
-    )
+    )*/
+
+console.log('keyup caracter: ',termoBusca)
+    this.subjectPesquisa.next(termoBusca)
   }
+
+
 }
